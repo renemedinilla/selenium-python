f0cf60ea076173946ad4074a89b4dd64282056f5
renelm <rene.lopez.medinilla@itzdata.tech>
2018-07-12 16:50:44 -0600

    unit test suite

--- chrome.py
+++ chrome.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 CHROME_PATH = '/usr/bin/google-chrome'
@@ -13,6 +14,13 @@ chrome_options.binary_location = CHROME_PATH
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
                           chrome_options=chrome_options
                          )  
-driver.get("https://www.google.com")
-driver.get_screenshot_as_file("capture.png")
+driver.get("http://www.python.org")
+driver.get_screenshot_as_file("title.png")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.ENTER)
+driver.get_screenshot_as_file("results.png")
+assert "No results found." not in driver.page_source
 driver.close()
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -11,17 +12,46 @@ firefox_options = Options()
 firefox_options.add_argument("--headless")  
 firefox_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 firefox_options.binary_location = FIREFOX_PATH
+ 
+class PythonOrgSearch(unittest.TestCase):
+    
+    def setUp(self):
+        self.driver = webdriver.Firefox(executable_path=FIREFOXDRIVER_PATH, firefox_options=firefox_options)
 
-driver = webdriver.Firefox(executable_path=FIREFOXDRIVER_PATH,
-                          firefox_options=firefox_options
-                         ) 
-driver.get("http://www.python.org")
-driver.get_screenshot_as_file("title.png")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.ENTER)
-driver.get_screenshot_as_file("results.png")
-assert "No results found." not in driver.page_source
-driver.close()
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        driver.get_screenshot_as_file("title.png")
+        assert "Python" in driver.title
+        elem = self.driver.find_element_by_name("q")
+        elem.clear()
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.ENTER)
+        driver.get_screenshot_as_file("results.png")
+        assert "No results found." not in driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+class AnotherTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.driver = webdriver.Firefox(executable_path=FIREFOXDRIVER_PATH, firefox_options=firefox_options)
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        driver.get_screenshot_as_file("title.png")
+        assert "Pythons" in driver.title
+        elem = self.driver.find_element_by_name("q")
+        elem.clear()
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.ENTER)
+        driver.get_screenshot_as_file("results.png")
+        assert "No results found." not in driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
