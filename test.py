1dda24a804269f1c68c0966f6d24fac7f1d9e328
renelm <rene.lopez.medinilla@itzdata.tech>
2018-07-12 12:47:44 -0600

    first test

---
+++ test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.ENTER)
+assert "No results found." not in driver.page_source
+driver.close()
